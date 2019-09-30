0631d97d2bd077b29e25ebcfcba44060de98a06a
VPAbraham <vpa456@gmail.com>
2019-09-30 17:40:16 -0600

    Set up boilerplate for News Article component

--- src/components/NewsArticle/NewsArticle.js
+++ src/components/NewsArticle/NewsArticle.js
@@ -1,6 +1,17 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './NewsArticle.css';
 
+class NewsArticle extends Component{
+  constructor() {
+    super()
+  }
+
+  render() {
+    return (
+      <h4>this is an article</h4>
+    )
+  }
+}
 // NEWSARTICLE COMPONENT CODE GOES HERE
 
 export default NewsArticle;
