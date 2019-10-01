c2c6464b9e411e3463eb86328bf3e9eadc3046c6
VPAbraham <vpa456@gmail.com>
2019-09-30 21:56:56 -0600

    Import icon images into Nav

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,5 +1,11 @@
 import React, { Component } from 'react';
 import './Menu.css'
+import NewsIcon from './menu-images/newspaper.png'
+import TechIcon from './menu-images/data.png'
+import EntIcon from './menu-images/video-camera.png'
+import SciIcon from './menu-images/laboratory.png'
+import HealthIcon from './menu-images/care.png'
+
 
 class Menu extends Component{
   constructor() {
@@ -8,25 +14,25 @@ class Menu extends Component{
 
   render() {
     return (
-      <nav>
-        <div className="nav--local">
-          <img></img>
+      <nav className="nav">
+        <div className="nav--local nav-div">
+          <img src={NewsIcon} className="icon"></img>
           <p>Local News</p>
         </div>
-        <div className="nav--technology">
-          <img></img>
+        <div className="nav--technology nav-div">
+          <img src={TechIcon} className="icon"></img>
           <p>Technology</p>
         </div>
-        <div className="nav--entertainment">
-          <img></img>
+        <div className="nav--entertainment nav-div">
+          <img src={EntIcon} className="icon"></img>
           <p>Entertainment</p>
         </div>
-        <div className="nav--science">
-          <img></img>
+        <div className="nav--science nav-div">
+          <img src={SciIcon} className="icon"></img>
           <p>Science</p>
         </div>
-        <div className="nav--health">
-          <img></img>
+        <div className="nav--health nav-div">
+          <img src={HealthIcon} className="icon"></img>
           <p>Health</p>
         </div>
       </nav>
