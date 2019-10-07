01da4584eae9a1076aa65ee12e83de9df32468fc
VPAbraham <vpa456@gmail.com>
2019-10-07 03:45:45 -0600

    Updates

---
+++ precache-manifest.2618ae6e38ae3fe59e2d9e3e8ad3202b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f96ea251b0b40d17b6f47a4077efdba6",
+    "url": "/VPAbraham/whats-new/index.html"
+  },
+  {
+    "revision": "f28f4870fb1b6b2195b9",
+    "url": "/VPAbraham/whats-new/static/css/main.bd20564b.chunk.css"
+  },
+  {
+    "revision": "0a5a97bead2df70e05f7",
+    "url": "/VPAbraham/whats-new/static/js/2.82a79f68.chunk.js"
+  },
+  {
+    "revision": "f28f4870fb1b6b2195b9",
+    "url": "/VPAbraham/whats-new/static/js/main.872f39f0.chunk.js"
+  },
+  {
+    "revision": "2666d35039b8e2c9034c",
+    "url": "/VPAbraham/whats-new/static/js/runtime-main.5a277a91.js"
+  }
+]);
